dd72ab6c09636479db2106685c1967f6f8b4fd15
지윤 <1yoouoo@gmail.com>
2025-10-28 15:10:03 +0900

    feat: add Microsoft Clarity tracking and robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,17 @@ export default function RootLayout({
             gtag('config', 'G-R9ZYC26CFJ');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tx3ohsch2z");
+          `}
+        </Script>
         {children}
       </body>
     </html>
