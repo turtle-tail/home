d9bf4edff718a0cece5eb3c2b119b61a362e2322
지윤 <1yoouoo@gmail.com>
2025-10-28 14:29:59 +0900

    Simplify homepage to Hello world

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import { projects } from '@/content/projects';
-import { ProjectGrid } from '@/components/project-grid';
-import { Header } from '@/components/header';
-
 export default function HomePage() {
   return (
-    <main className="min-h-screen bg-gray-50">
-      <div className="container mx-auto px-4 py-16 max-w-7xl">
-        <Header />
-        <ProjectGrid projects={projects} />
-      </div>
+    <main>
+      <h1>Hello world!</h1>
     </main>
   );
 }
